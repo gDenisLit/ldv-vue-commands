82c43a98b2ec26562192a3966827f034c6abe662
Denis Lit <denislitviak@gmail.com>
2023-01-20 12:49:20 +0200

    add version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { resolve, join } = require('node:path')
 const program = require('commander')
 
 program
-    .version('1.6.0')
+    .version('1.7.0')
     .description('Simple cli commands to generate components and views in vue projects')
 
 program
